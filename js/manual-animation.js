c6438901932858744fdf69b46fd300e045ea4f89
Allen Fulmer <allenful@uw.edu>
2022-05-26 15:32:09 -0700

    Added rotation when hovering over the ship's image a "Forward" button that sends the ship in whatever direction it's facing.

--- js/manual-animation.js
+++ js/manual-animation.js
@@ -22,22 +22,35 @@ function moveImage(direction) {
     let imgObjStyle = imgObj.style;
     let topVal = parseInt(imgObjStyle.top, 10);
     let leftVal = parseInt(imgObjStyle.left, 10);
+    let speed = 30;
 
     if(direction === 'KeyA') {
-        imgObjStyle.left = (leftVal - 30) + "px";
-    } 
+        imgObjStyle.left = (leftVal - speed) + "px";
+//        imgObjStyle.left = (leftVal - deltaX) + "px";
+    }
 
     if(direction === 'KeyW') {
-        imgObjStyle.top = (topVal - 30) + "px";
+        imgObjStyle.top = (topVal - speed) + "px";
+//        imgObjStyle.top = (topVal - deltaY) + "px";
     }
 
     if(direction === 'KeyD') {        
-        imgObjStyle.left = (leftVal + 30) + "px";
+        imgObjStyle.left = (leftVal + speed) + "px";
+//        imgObjStyle.left = (leftVal + deltaX) + "px";
     }
 
     
     if(direction === 'KeyS') {
-        imgObjStyle.top = (topVal + 30) + "px";
+        imgObjStyle.top = (topVal + speed) + "px";
+//        imgObjStyle.top = (topVal + deltaY) + "px";
+    }
+
+    if(direction === 'Forward') {
+        let angle = currentDegrees * Math.PI / 180; // Degrees to Radians
+        let deltaX = Math.sin(angle) * speed;
+        let deltaY = Math.cos(angle) * speed;
+        imgObjStyle.top = (topVal - deltaY) + "px";
+        imgObjStyle.left = (leftVal + deltaX) + "px";
     }
 }
 
@@ -46,7 +59,24 @@ function init() {
     imgObj.style.position = 'relative';
     imgObj.style.left = '0px';
     imgObj.style.top = '0px';
+
+    // Additional Listeners
+    currentDegrees = 0;
+    degreeIncrement = 1;
+    imgObj.addEventListener("click", function() {degreeIncrement = 0 - degreeIncrement; });
+    imgObj.addEventListener("pointerover", function() {rotateShip(); });
+    imgObj.addEventListener("pointerout", function() { clearInterval(hoverForward); });
 }
+
+function rotateShip()
+{
+    hoverForward = setInterval( function() {
+    currentDegrees += degreeIncrement;
+//    console.log("Ship rotated to " + currentDegrees + " degrees");
+    imgObj.style.transform = 'rotate(' + currentDegrees + 'deg)';
+    }, 10);
+}
+
 
 
 window.onload = init;
